780203602fbbf0c2b5623bf8ee09fc8f19bea280
Nazmi <nazmifeeroz@hotmail.com>
2019-06-26 23:50:55 +0800

    materialize css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="row">
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col s6">
+            <input
+              placeholder="Placeholder"
+              id="first_name"
+              type="text"
+              class="validate"
+            />
+            <label for="first_name">First Name</label>
+          </div>
+          <div class="input-field col s6">
+            <input id="last_name" type="text" class="validate" />
+            <label for="last_name">Last Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input
+              disabled
+              value="I am not editable"
+              id="disabled"
+              type="text"
+              class="validate"
+            />
+            <label for="disabled">Disabled</label>
+          </div>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
