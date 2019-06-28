511a801900bcde589ddedbcea47b185857d43c11
Nazmi <nazmifeeroz@hotmail.com>
2019-06-28 09:59:55 +0800

    use Context

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import AppContext from './components/context'
+
+const Main => (
+  <AppContext.provider>
+    <App />
+  </AppContext.provider>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
