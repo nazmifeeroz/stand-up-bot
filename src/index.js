c44c88e21c272de16f63acf43a4a2669cf7cc54c
Nazmi <nazmifeeroz@hotmail.com>
2019-07-20 22:04:02 +0800

    :construction: sidekick wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Sidekick from './Sidekick'
 import StoreProvider from './services/store'
 
 console.log(
@@ -8,9 +9,11 @@ console.log(
   'font-weight: bold; font-size: 20px;color: blue; text-shadow: 3px 3px 0 rgb(217,31,38)'
 )
 
+console.log('location.pathname', window.location.pathname)
+
 ReactDOM.render(
   <StoreProvider>
-    <App />
+    {window.location.pathname === '/sidekick' ? <Sidekick /> : <App />}
   </StoreProvider>,
   document.getElementById('root')
 )
